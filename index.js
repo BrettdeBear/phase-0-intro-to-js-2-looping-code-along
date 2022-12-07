d7186670ee90a9ee7271548083d4c9b8fe9ec10e
Brett de Bear <bkdebear@gmail.com>
2022-12-07 10:47:36 -0800

    commit tree dcd9864c7d0da9bac512b64e56ae3a3b1d2910c4

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(thank, event) {
+    const finishedCards = [];
+    for(let i = 0; i < thank.length; i++) {
+       finishedCards.push(`Thank you, ${thank[i]}, for the wonderful ${event} gift!`);
+    }
+    return finishedCards;
+}
+
+
+function countDown(count) {
+    while (count >= 0) {
+        console.log(count--);
+    }
+    return countDown;
+}

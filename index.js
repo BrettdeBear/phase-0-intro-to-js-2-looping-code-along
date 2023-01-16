da6defc4b9f9c682dfa71400ce13bb7677f0a7c9
Brett de Bear <bkdebear@gmail.com>
2023-01-15 17:04:39 -0800

    finished with looping lab yay

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+let messages = [];
+
+function writeCards(name, event) {
+    for (let i = 0; i < name.length; i++) {
+       messages.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+    }
+    return messages;
+}
+
+function countDown(i) {
+    while (i >= 0) {
+        console.log(i--);
+    }
+}
